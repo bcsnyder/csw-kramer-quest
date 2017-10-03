49703759ee6a49989e7f62e91e3028965fd01cc4
bsnyderman <bensnyder888@gmail.com>
2017-10-03 10:53:39 -0400

    t dddq opjfhkjdsj# Your branch is up-to-date with 'origin/master'.

--- Monster.java
+++ Monster.java
@@ -1,26 +1,27 @@
-import javax.swing.JOptionPane;
-/**
- * Abstract class Monster - This class holds all of the enemy types you might encounter during the game.  
- * This class will have the ability to check whether its alive or not, a set amount of health, a set amount of
- * attack power, and a set amount of defense.  
- * -removed abstract
- * @author Dylan, Ben
- * @version 1.2
- */
-public class Monster
-{
-    String name = "Gremlin";
-    String symbol = "G";
-    int health = 80; //NO need to have doubles
-    int atk = Integer.parseInt(JOptionPane.showInputDialog("Enemy's atk:"));
-    private double def = 1;
 
+public class Gremlin extends Monster
+{
+    String name;
+    String symbol;
+    int health; //NO need to have doubles
+    int atk;
+    int xPos;
+    int yPos;
+
+    public Gremlin() {
+        symbol = "G";
+        name = "Gremlin";
+        health = (int)(Math.random()*5 + 10);
+        atk = (int)(Math.random()*2 + 1);
+    }
+    
     /**
      * When called, this method will subtract the appropriate amount of health from the monster.  If the monster
-     * is alive, it returns 1.  If the monster is dead it returns 0
+     * is alive, it returns true.  If the monster is dead it returns false.
      * 
      */
-    public boolean ouchie(int y)
+    
+	public boolean ouchie(int y)
     {
         health = health - y;
         if (health <= 0){
@@ -28,6 +29,19 @@ public class Monster
         }else{
             return true;
         }
+    }
+    
+    public int getX() {
+        return xPos;
+    }
+    
+    public int getY() {
+        return yPos;
+    }
+    
+    public void setPos(int x, int y) {
+        xPos = x;
+        yPos = y;
     }
     
     public String getName() {
