d121395b34d96ccfaebad4fb421fb7a8f4c5b9ae
Emily-Oldham <31962054+Emily-Oldham@users.noreply.github.com>
2017-10-18 14:18:36 -0400

    Update Stage.java

--- Stage.java
+++ Stage.java
@@ -14,11 +14,40 @@ public class Stage
     {
         for (int counter = 0; counter < size; counter++)
         {
+            
             int min = 7 + counter/2;
             int max = 11 + counter/2;
             Room add = new Room(counter, min, max);
-            add.fill();
+            add.fillDots();
+            add.fillWalls();
+            add.fillSymbols();
+            
+            //This adds the back door so that the player can go through this door
+            int height = add.getHeight();
+            int width = add.getWidth();
+            if (counter != 0){
+                if (doorWallLast == 1 || doorWallLast == 3){
+                    yCoor = (int) (Math.random () * (width - 2)) + 2;
+                }else if (doorWallLast == 2 || doorWallLast == 4){
+                    xCoor = (int) (Math.random () * (height - 2)) + 2;
+                }
+                add.addBackDoor (doorWallLast,yCoor, xCoor);
+            }
+            System.out.println (doorWallLast);
+            doorWallLast = add.returnDoorWall();
+            if (doorWallLast == 1){
+                doorWallLast = 3;
+            }else if (doorWallLast == 2){
+                doorWallLast = 4;
+            }else if (doorWallLast == 3){
+                doorWallLast = 1;
+            }else if (doorWallLast == 4){
+                doorWallLast = 2;
+            }
+            System.out.println (doorWallLast);
             floor[counter] = add;
+        }
+        
         }
     }
    
