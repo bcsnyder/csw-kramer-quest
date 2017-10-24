a23b2374493ae99d5d9a56ff8fa3de1e3b135fdc
arushitayal <arushitayal@gmail.com>
2017-10-24 14:02:57 -0400

    Update MenuWindow.java
    Fixed the filename

--- MenuWindow.java
+++ MenuWindow.java
@@ -66,7 +66,7 @@ public class MenuWindow extends JFrame
             
         //loads image
         try {
-            imgLogo = ImageIO.read(new File("/Users/Ben/Desktop/School/SE/csw-kramer-quest/images/logo_ex.jpg"));
+            imgLogo = ImageIO.read(new File(imgLogoFilename));
         } catch (IOException e) {}
         
         //Sets up where all the components in the container go
