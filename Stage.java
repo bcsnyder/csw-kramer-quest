84f9f625c050525d9be2a818013264566e95a47a
Emily-Oldham <31962054+Emily-Oldham@users.noreply.github.com>
2017-10-24 08:34:45 -0400

    Update Stage.java

--- Stage.java
+++ Stage.java
@@ -1,26 +1,33 @@
 public class Stage 
 {
-    private int size;
+    private int level;
+    private int min;
+    private int max;
+    private int numRooms;
     private Room[] floor;
+    private int xCoor = 0;      
+    private int yCoor = 0;
+    private int doorWallLast = 0; 
     
-    public Stage(int numRooms)
+    public Stage(int num)
     {
-        size = numRooms;
+        level = num;
+        min = 6; //can only be so small 
+        max = 20;//can only be soo big, it needs to fit on screen
+        numRooms = (level * 2) + 2;
         floor = new Room[numRooms];
-        makeRooms();
     }
     
     public void makeRooms() 
     {
-        for (int counter = 0; counter < size; counter++)
+        for (int counter = 0; counter < numRooms; counter++)
         {
+            Room add = new Room(level, min, max);
+            add.fillDots();
+            add.fillWalls();
+            add.fillSymbols();
             
-            int min = 7 + counter/2;
-            int max = 11 + counter/2;
-            Room add = new Room(counter, min, max);
-            add.fill();
-            
-            //This adds the back door so that the player can go through this door
+            //This adds the back door so that the player can go through this 
             int height = add.getHeight();
             int width = add.getWidth();
             if (counter != 0){
@@ -31,7 +38,7 @@ public class Stage
                 }
                 add.addBackDoor (doorWallLast,yCoor, xCoor);
             }
-            System.out.println (doorWallLast);
+            
             doorWallLast = add.returnDoorWall();
             if (doorWallLast == 1){
                 doorWallLast = 3;
@@ -42,11 +49,11 @@ public class Stage
             }else if (doorWallLast == 4){
                 doorWallLast = 2;
             }
-            System.out.println (doorWallLast);
+            
             floor[counter] = add;
         }
         
-        }
+    }
    
     public String toString() 
     {
@@ -58,17 +65,16 @@ public class Stage
         return output;
     }
     
-    public Room getRoom(int num) {
+     public Room getRoom(int num) {
         return floor[num];
     }
     
     public Room nextRoom (int roomNum){
          return floor[roomNum];
     }
-    
+     
     //floor [0] = one;  
     //floor [1] = two;
     //floor [2] = three;
     //floor [3] = four;
 }
-
