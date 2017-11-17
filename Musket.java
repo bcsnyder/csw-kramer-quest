b1f2dca73abe55716ad6b111e45eafea1cff397a
bsnyderman <bensnyder888@gmail.com>
2017-11-17 10:21:12 -0500

    Updated tile system for new weapons

--- Musket.java
+++ Musket.java
@@ -6,36 +6,42 @@ public class Musket extends Weapon
     private String name;
     private int damage;
     private int durability;
-    
+    String category;
+
     public Musket() {
-       type = "Weapon";
-       name = "Musket";
-       damage = 25;
-       durability = 1;
-       symbol = '$';
+        type = "Weapon";
+        name = "Musket";
+        category = "Item";
+        damage = 25;
+        durability = 1;
+        symbol = '$';
     }
     
+    public String getCategory() {
+        return category;
+    }
+
     public String getType() {
         return type;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public int attack() {
         durability -= 1;
         return damage;
     }
-    
-        public int getDamage() {
+
+    public int getDamage() {
         return damage;
     }
-    
+
     public int getDurability() {
         return durability;
     }
-    
+
     public char getSymbol() {
         return symbol;
     }
--- Sword.java
+++ Sword.java
@@ -6,36 +6,42 @@ public class Sword extends Weapon
     private String name;
     private int damage;
     private int durability;
-    
+    String category;
+
     public Sword() {
-       type = "Weapon";
-       name = "Sword";
-       damage = 15;
-       durability = 4;
-       symbol = '$';
+        type = "Weapon";
+        name = "Sword";
+        category = "Item";
+        damage = 15;
+        durability = 4;
+        symbol = '$';
     }
     
+    public String getCategory() {
+        return category;
+    }
+
     public String getType() {
         return type;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public int attack() {
         durability -= 1;
         return damage;
     }
-    
-        public int getDamage() {
+
+    public int getDamage() {
         return damage;
     }
-    
+
     public int getDurability() {
         return durability;
     }
-    
+
     public char getSymbol() {
         return symbol;
     }
