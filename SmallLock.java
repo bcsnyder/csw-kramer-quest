ce8a1bef1dc6d44a710f2076205fd607319625d7
arushitayal <arushitayal@gmail.com>
2017-12-04 08:52:50 -0500

    Add files via upload
    Some treasures lock, are opened with a key

---
+++ SmallLock.java
@@ -0,0 +1,35 @@
+/**
+ * A simple subclass of Item that is a key. Used
+ * to display to user the inventory screen.
+ */
+public class SmallLock extends Item
+{
+    private char symbol;
+    private String type;
+    private String name;
+    String category;
+
+    public SmallLock() {
+        type = "Lock";
+        category = "Item";
+        symbol = '$';
+    }
+
+    public String getCategory() {
+        return category;
+    }
+    
+    public String getType()
+    {
+        return type;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public boolean unlock() {
+        return true;
+    }
+}
