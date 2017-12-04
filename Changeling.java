424ae6928b99cfb336556d291dc2addd4ec40725
arushitayal <arushitayal@gmail.com>
2017-12-04 08:30:51 -0500

    Update Changeling.java
    Changeling appears to be treasure

--- Changeling.java
+++ Changeling.java
@@ -9,11 +9,11 @@ public class Changeling extends Monster implements Tileable
     String category;
 
     public Changeling() {
-        symbol = '?';
+        symbol = '$';
         name = "Changeling";
         category = "Monster";
-        health = (int)(Math.random()*101);
-        atk = (int)(Math.random()*31);
+        health = (int)(Math.random()*101)+1;
+        atk = (int)(Math.random()*31)+1;
     }
 
     public String getCategory() {
@@ -23,9 +23,7 @@ public class Changeling extends Monster implements Tileable
     /**
      * When called, this method will subtract the appropriate amount of health from the monster.  If the monster
      * is alive, it returns true.  If the monster is dead it returns false.
-     * 
      */
-
     public boolean ouchie(int y)
     {
         health = health - y;
@@ -45,54 +43,6 @@ public class Changeling extends Monster implements Tileable
     }
 
     public Room chooseMove(Room tiles) {
-        int newX;
-        int newY;
-        boolean outOfMoves = false;
-        ArrayList directions = new ArrayList();
-
-        for(int i = 1; i<=4; i++) {
-            directions.add(i);
-        }
-
-        do {
-            int directionChooser = (int)(Math.random()*directions.size());
-            newX = x;
-            newY = y;
-
-            if(directions.isEmpty()) {
-                outOfMoves = true;
-                break;
-            }
-
-            int chosenDirection = (int)directions.get(directionChooser);
-
-            if (chosenDirection == 1) {
-                newY = y - 1;
-                directions.remove(directions.indexOf(1));
-            } else if(chosenDirection == 2) {
-                newY = y + 1;
-                directions.remove(directions.indexOf(2));
-            } else if(chosenDirection == 3) {
-                newX = x - 1;
-                directions.remove(directions.indexOf(3));
-            } else {
-                newX = x + 1;
-                directions.remove(directions.indexOf(4));
-            }
-            if (tiles.getTile(newX,newY) == '@') {
-                return null;
-            }
-        } while (tiles.getTile(newX,newY) != '.');
-
-        if(!outOfMoves) {
-            Tileable thisMonster = tiles.removeTile(x,y);
-            x = newX;
-            y = newY;
-            tiles.addTile(x,y,thisMonster);
-        }
-        
-        moved = true;
-        
         return tiles;
     }
 
