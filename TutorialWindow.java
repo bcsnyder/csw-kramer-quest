4801ca44289bf876b97295f7321666ef9bb122e6
bsnyderman <bensnyder888@gmail.com>
2017-12-14 09:07:11 -0500

    Started working on in depth tutorial

--- TutorialWindow.java
+++ TutorialWindow.java
@@ -15,12 +15,13 @@ import javax.swing.*;
 public class TutorialWindow extends JFrame
 {
     //Sets size of window
-    public static final int CANVAS_WIDTH  = 800;
+    public static final int CANVAS_WIDTH  = 1000;
     public static final int CANVAS_HEIGHT = 500;
 
     MenuWindow menuWind;
 
     private String[] tutorial;
+    private int pageNumber;
     private TutorialText canvas;
 
     /**
@@ -31,16 +32,32 @@ public class TutorialWindow extends JFrame
         canvas = new TutorialText();    
         canvas.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
 
+        pageNumber = 1;
+        
         JPanel buttonPane = new JPanel(new FlowLayout());
         JButton menuButton = new JButton("Return to Menu ");
         buttonPane.add(menuButton);
         menuButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
-                menuWind = new MenuWindow();
-                dispose();
-                menuWind.displayWindow(musicPlayer);
-            }
-        });
+                public void actionPerformed(ActionEvent evt) {
+                    menuWind = new MenuWindow();
+                    dispose();
+                    menuWind.displayWindow(musicPlayer);
+                }
+            });
+
+        addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyPressed(KeyEvent evt) {
+                    switch(evt.getKeyCode()) {
+                        case KeyEvent.VK_ENTER:
+                        if (pageNumber < 5) {
+                            pageNumber++;
+                            repaint();
+                        }
+                        break;
+                    }
+                }
+            });
 
         //Sets the Drawing JPanel as the JFrame's content-pane
         Container cp = getContentPane();
@@ -52,22 +69,34 @@ public class TutorialWindow extends JFrame
         pack(); //Either pack() the components or setSize()
         setTitle("Tutorial"); //JFrame sets the title of outer frame
         setVisible(true); //Displays window
+        setFocusable(true);
         setLocationRelativeTo(null); //Puts the JFrame in the middle of the screen @Francis
     }
 
     /**
      * Sets the text line by line for the tutorial as an array
      */
-    private void setTutorial() {
-        tutorial = new String[8];
-        tutorial[0] = "Hello adventurer, welcome to the fantastic world of wefwef.";
-        tutorial[1] = "You are stranded in a dark dungeon and must find the idol of ___ on the lowest floor";
-        tutorial[2] = "Along the way you will find treasure ($) and fight monsters (text characters).";
-        tutorial[3] = "Use the WASD keys to move around or select different options on the menu.";
-        tutorial[4] = "Use enter to confirm your selection. # is a door forward, ^ is a door backward.";
-        tutorial[5] = "Press i to open your inventory. As you travel your stamina will deplete.";
-        tutorial[6] = "Food will replenish your stamina, but if it gets too low you could starve.";
-        tutorial[7] = "Fighting monsters will increase your maximum health and potions will heal you.";
+    private void setTutorialBody(int page) {
+        if (page == 1) {
+            tutorial = new String[8];
+            tutorial[0] = "Hello adventurer, welcome to the fantastic world of wefwef.";
+            tutorial[1] = "It is a world full of evil monsters and items of great magical power.";
+            tutorial[2] = "You have been searching around the ruins of a keep for an ancient idol.";
+            tutorial[3] = "Suddenly the floor collapses and you fall into some buried chamber, dazed.";
+            tutorial[4] = "When you regain your senses, you don't see the hole you fell into.";
+            tutorial[5] = "Looking around, you see the room you are in is crumbling but well kept.";
+            tutorial[6] = "There are lit torches in the walls and you see an eerie humanoid shadow.";
+            tutorial[7] = "You have a strange feeling that the idol you're searching for is nearby...";
+        } else if (page == 2) {
+            tutorial = new String[7];
+            tutorial[0] = "Hello adventurer, welcome to the fantastic world of wefwef.";
+            tutorial[1] = "It is a world full of evil monsters and items of great magical power.";
+            tutorial[2] = "You have been searching around the ruins of a keep for an ancient idol.";
+            tutorial[3] = "Suddenly the floor collapses and you fall into some buried chamber, dazed.";
+            tutorial[4] = "When you regain your senses, you don't see the hole you fell into.";
+            tutorial[5] = "Looking around, you see the room you are in is crumbling but well kept.";
+            tutorial[6] = "There are lit torches in the walls and you see an eerie humanoid shadow.";
+        }
     }
 
     /**
@@ -89,19 +118,44 @@ public class TutorialWindow extends JFrame
         public void paintComponent(Graphics g) {
             super.paintComponent(g); //Paints base background
             setBackground(Color.BLACK); //Sets background color for this JPanel
+            int x;
+            String txt;
+
+            if (pageNumber == 1) {
+                g.setColor(Color.WHITE);
+                g.setFont(new Font("Monospaced", Font.PLAIN, 24));
+                txt = "The world of Wefwef:";
+                x = centerStringX(txt, CANVAS_WIDTH, g);
+                g.drawString(txt, x, 20);
+
+                setTutorialBody(1);
+                g.setFont(new Font("Monospaced", Font.PLAIN, 18));
+                for (int i = 0; i < tutorial.length; i++) {
+                    txt = tutorial[i];
+                    x = centerStringX(txt, CANVAS_WIDTH, g);
+                    g.drawString(txt, x, (50 + 30*i));
+                }
+            } else if (pageNumber == 2) {
+                g.setColor(Color.WHITE);
+                g.setFont(new Font("Monospaced", Font.PLAIN, 24));
+                txt = "Moving through the dungeon:";
+                x = centerStringX(txt, CANVAS_WIDTH, g);
+                g.drawString(txt, x, 20);
+
+                setTutorialBody(2);
+                g.setFont(new Font("Monospaced", Font.PLAIN, 18));
+                for (int i = 0; i < tutorial.length; i++) {
+                    txt = tutorial[i];
+                    x = centerStringX(txt, CANVAS_WIDTH, g);
+                    g.drawString(txt, x, (50 + 30*i));
+                }
+            }
 
             g.setColor(Color.WHITE);
             g.setFont(new Font("Monospaced", Font.PLAIN, 14));
-            setTutorial();
-            String txt;
-            int x;
-            
-            //Displays the tutorial text line by line, however long it is
-            for (int i = 0; i < tutorial.length; i++) {
-                txt = tutorial[i];
-                x = centerStringX(txt, CANVAS_WIDTH, g);
-                g.drawString(txt, x, (30 + 25*i));
-            }
+            txt = "Page "+pageNumber+" out of 4. Press enter to continue.";
+            x = centerStringX(txt, CANVAS_WIDTH, g);
+            g.drawString(txt, x, CANVAS_HEIGHT-30);
         }
     }
 }
