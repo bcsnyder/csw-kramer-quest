37d311998e1848edd376469cbb5dda15c0c0c5be
chenchristophercsw <31961010+chenchristophercsw@users.noreply.github.com>
2018-01-03 08:37:53 -0500

    Update Skeleton.java

--- Skeleton.java
+++ Skeleton.java
@@ -29,6 +29,9 @@ public class Skeleton extends Monster implements Tileable
     public boolean ouchie(int y)
     {
         health = health - y;
+        if (health < 0) {
+            health = 0;
+        }
         if (health <= 0){
             return false;
         }else{
